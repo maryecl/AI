b2e17778d2335f9307ee659fab9c5925df6beb5b
Lidiya Hevko <lidiyahevko@poblenou-137-252.eduroamcp.upf.edu>
2018-01-30 16:31:38 +0100

    BFS,DFS,DIJKSTRA

--- search/search.py
+++ search/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -87,16 +87,60 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
+    print "*********************"
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    stack = util.Stack() # we create a Stack, empty
+    visited =[] # a list for visited nodes, an empty array
+    stack.push((problem.getStartState(),[]))#here we add first/start node to Stack
+    while not stack.isEmpty():
+        state,actions=stack.pop()#returns us the most upper node of the stack and after removes it
+        for successors in problem.getSuccessors(state):#we are in some node, which has some adjacent nodes to him
+            state = successors[0] #the actual node, where we are now
+            direction = successors[1]#the node adjacent to the actual node, the node where we have to go next
+            if state not in visited:#we check if the node we are is already visited or not
+                if problem.isGoalState(state):#we check if the state we are is the goal of the problem or not
+                    return actions + [direction]#if we are in the last node-goal, we have to retrun
+                else:#if not we have to add visited nodes to our list,and to the stack
+                    stack.push( (state, actions + [direction]) )
+                    visited.append( state )
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+    queue= util.Queue()
+    close =[]
+    queue.push((problem.getStartState(),[]))
+    while not queue.isEmpty():
+        state, actions = queue.pop()
+        for i in problem.getSuccessors(state):
+            state = i[0]
+            direction = i[1]
+            if state not in close:
+                if problem.isGoalState(state):
+                    return actions + [direction]
+                else:
+                    queue.push( (state, actions + [direction]) )
+                    close.append( state )
+
     util.raiseNotDefined()
 
-def uniformCostSearch(problem):
+def uniformCostSearch(problem):#DIJKSTRA
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    queue = util.PriorityQueue()
+    visited = []
+    queue.push((problem.getStartState(), [], 0), 0)
+    while not queue.isEmpty():
+        currentState, currentPath, currentCost = queue.pop()
+        if problem.isGoalState(currentState):
+            return currentPath
+        for state, direction, cost in problem.getSuccessors(currentState):
+            if state not in visited:
+                queue.push((state, currentPath + [direction], currentCost + cost), currentCost + cost)
+                visited.append(state)
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
